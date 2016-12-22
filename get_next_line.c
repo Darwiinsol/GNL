bbd766fd266b7927b70edad5788e0812fa1cb632
Arthur PISSIER <apissier@e3r13p2.42.fr>
2016-12-22 19:13:28 +0100

    1 leaks

---
+++ get_next_line.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line7.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apissier <apissier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/21 18:46:55 by apissier          #+#    #+#             */
+/*   Updated: 2016/12/22 18:24:41 by apissier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int				check_end_of_line(char **save, char **line)
+{
+	char				*tmp;
+	int					i;
+
+	i = 0;
+	tmp = NULL;
+	if (*save)
+	{
+		if (ft_strchr(*save, '\n') != NULL)
+		{
+			tmp = ft_strchr(*save, '\n');
+			*tmp = '\0';
+			while (save[i])
+			{
+				if (*save[i] == '\n')
+					*save[i] = '\0';
+				i++;
+			}
+			if (!(*line = ft_strdup(*save)))
+				return (0);
+			if (!(*save = ft_strdup(&tmp[1])))
+				return (0);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+static int				while_read(const int fd, char *buf, char **save, \
+						char **line)
+{
+	int					ret;
+	char				*ptr;
+
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		ptr = ft_strjoin(*save, buf);
+		if (!ptr)
+			return (-1);
+		free(*save);
+		*save = ptr;
+		if (check_end_of_line(save, line))
+			return (1);
+	}
+	if (ret < 0)
+		return (-1);
+	return (0);
+}
+
+int						get_next_line(const int fd, char **line)
+{
+	char				buf[BUFF_SIZE + 1];
+	static char			*save;
+
+	if (check_end_of_line(&save, line))
+		return (1);
+	if (!save)
+		save = ft_strnew(1);
+	if (while_read(fd, buf, &save, line) > 0)
+		return (1);
+	else if (while_read(fd, buf, &save, line) < 0)
+		return (-1);
+	if (save)
+	{
+		*line = ft_strdup(save);
+		if (ft_strlen(save) == 0)
+			return (0);
+		save = NULL;
+		return (1);
+	}
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apissier <apissier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 15:51:14 by apissier          #+#    #+#             */
+/*   Updated: 2016/12/22 18:50:38 by apissier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1000 
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "./libft/libft.h"
+
+int			get_next_line(const int fd, char **line);
+
+#endif
